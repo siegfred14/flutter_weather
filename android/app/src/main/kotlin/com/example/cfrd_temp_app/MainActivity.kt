a61ed5a7bab62afc4d327269245e285c74f539ac
Siegfred Samson <siegfredsamson@gmail.com>
2022-10-20 19:01:54 +0100

    flutter temp app initial setup

---
+++ android/app/src/main/kotlin/com/example/cfrd_temp_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cfrd_temp_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class XColors {
+  static Color accentColor = const Color(0xff2249AE);
+  static Color secondaryColor = const Color(0xff66AC4C);
+  static Color secondaryColor2 = const Color(0xffB3CEE2);
+  static Color secondaryLight = const Color(0xffAF03A0);
+  static Color background() => const Color(0xffFFFFFF);
+  static Color white() => const Color(0xffFFFFFF);
+}
