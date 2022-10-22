292567d8668fcd16d24158ff9ab10c4b8c333c2a
Siegfred Samson <siegfredsamson@gmail.com>
2022-10-22 13:45:19 +0100

    Splash screen completed

--- lib/ui/splash/splash.dart
+++ lib/ui/splash/splash.dart
@@ -6,16 +6,9 @@ class Splash extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color(0xff0C1823),
+        backgroundColor: Color(0xff0c1823),
         body: Center(
-          child: Container(
-            child: const Text('Cfrd Meteorology',
-                style: TextStyle(
-                    fontSize: 27.4,
-                    color: Color(0xffFFFFFF),
-                    fontFamily: "Syncopate",
-                    fontWeight: FontWeight.w700)),
-          ),
+          child: Image.asset('assets/images/ccn_flutter_splash.png'),
         ));
   }
 }
