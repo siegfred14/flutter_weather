beddb09ff08cd17f0fe7cfebd000c01e0315c6e5
Siegfred Samson <siegfredsamson@gmail.com>
2022-10-23 13:38:13 +0100

    ccn_flutter_splash2_1.png added

---
+++ lib/ui/splash/Splash2.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Splash2 extends StatefulWidget {
+  const Splash2({Key? key}) : super(key: key);
+
+  @override
+  State<Splash2> createState() => _Splash2State();
+}
+
+class _Splash2State extends State<Splash2> {
+  @override
+  Widget build(BuildContext context) {}
+}
