fd01f82f02c9b3c5074ad29e26bdb6426f5c103c
Siegfred Samson <siegfredsamson@gmail.com>
2022-10-25 13:24:45 +0100

    new images for homepage added and homepage in progress

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -18,13 +18,55 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Consumer<HomeViewModel>(builder: (context, model, child) {
       return Scaffold(
-        body: model.isLoading!
-            ? Container(
-                child: Text('Loading...'),
-              )
-            : Container(
-                child: Text(model.weatherModel!.main!.temp.toString()),
+        // App bar
+        appBar: AppBar(
+          title: const Text(
+            "Quebec-Canada",
+            style: TextStyle(
+                color: Color(0xff36424D),
+                fontFamily: "Poppins",
+                fontWeight: FontWeight.w400,
+                fontSize: 12),
+          ),
+          leading: Image.asset("assets/images/rectangle10.png"),
+        ),
+        // Body
+        body: Container(
+            child: Column(
+          children: [
+            model.isLoading!
+                ? const Text('Loading...')
+                : Text(model.weatherModel!.main!.temp.toString()),
+            // Space
+            const SizedBox(
+              height: 110,
+            ),
+            // FeelsLike
+            const Text.rich(
+              TextSpan(
+                children: <InlineSpan>[
+                  TextSpan(text: "Feels Like A Good\nTime to ..."),
+                  WidgetSpan(
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(5, 0, 0, 5),
+                      child: Image.asset(
+                        "assets/images/rectangle10.png",
+                        width: 30,
+                      ),
+                    ),
+                  ),
+                ],
               ),
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                fontSize: 45,
+                color: Color(0xff0C1823),
+                fontFamily: 'poppins',
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ],
+        )),
       );
     });
   }
