8b488497f43c3b274d674b694e33497db2c81b76
Siegfred Samson <siegfredsamson@gmail.com>
2022-10-28 21:41:31 +0100

    initial dropdownbotton

--- lib/services/endpoints.dart
+++ lib/services/endpoints.dart
@@ -1,4 +1,4 @@
 class Endpoints {
-  static String weatherAPI({String? cityName, String? appID}) =>
-      'https://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=$appID&units=metric';
+  static String weatherAPI({String? location, String? appID}) =>
+      'https://api.openweathermap.org/data/2.5/weather?q=$location&appid=$appID&units=metric';
 }
